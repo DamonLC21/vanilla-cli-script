bbe9fdc5d9a3917f96adba4557a487b099b99e84
DamonLC21 <chiversdamon@gmail.com>
2020-10-06 14:01:42 -0600

    create Directory function added

--- vanilla.js
+++ vanilla.js
@@ -1,3 +1,19 @@
 #! /usr/bin/env node 
+const fs = require('fs')
+const path = `/${process.argv[2] }`
 
-console.log('Creating files with node.js -- Loading') 
+console.log('Creating project with node.js -- Loading', path) 
+
+const createDirError = error => {
+    if(error){
+        console.log('An error occured: ', error)
+    } else {
+        console.log(`Your directory had been created! Try <cd ${path}> to access your project. `)
+    }
+}
+
+const createDir = directory => {
+    fs.mkdirSync(process.cwd() + directory, { recursive: true }, createDirError)
+}
+
+createDir(path)
