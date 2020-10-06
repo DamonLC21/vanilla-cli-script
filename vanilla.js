6cd11a82f3d79eeb1c62859e5fc83afbb330aef8
DamonLC21 <chiversdamon@gmail.com>
2020-10-06 14:29:51 -0600

    create File function added

--- vanilla.js
+++ vanilla.js
@@ -1,19 +1,36 @@
 #! /usr/bin/env node 
 const fs = require('fs')
-const path = `/${process.argv[2] }`
+const projectName = process.argv[2]
+const path = `/${projectName}`
 
-console.log('Creating project with node.js -- Loading', path) 
+console.log('Creating project with node.js -- Loading') 
 
 const createDirError = error => {
     if(error){
         console.log('An error occured: ', error)
     } else {
-        console.log(`Your directory had been created! Try <cd ${path}> to access your project. `)
+        console.log(`Your project has been created! Try <cd ${projectName}> to access your project. `)
     }
 }
 
+const createFileError = error => {
+    if(error){
+        console.log('An error occured: ', error)
+    } else {
+        console.log(`Your project has been created! Try <cd ${projectName}> to access your project. `)
+    }
+}
 const createDir = directory => {
     fs.mkdirSync(process.cwd() + directory, { recursive: true }, createDirError)
 }
 
-createDir(path)
+const createFile = (fileName, content) => {
+    fs.writeFile(`./${projectName}/${fileName}`, content, createFileError)
+}
+
+if(projectName){
+    createDir(path)
+    createFile('index.js', 'console.log(\"Hello World\")')
+} else {
+    console.log('You must add the project name as an argument. ex. vanilla <project-name>')
+}
