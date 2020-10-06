b17505e63728062bd709c81e900ebf4970060797
DamonLC21 <chiversdamon@gmail.com>
2020-10-06 15:38:08 -0600

    creates three files, index.html main.css app.js, and displays out put to console.

--- vanilla.js
+++ vanilla.js
@@ -1,7 +1,21 @@
 #! /usr/bin/env node 
 const fs = require('fs')
+const { resolve } = require('path')
 const projectName = process.argv[2]
 const path = `/${projectName}`
+const boilerHTML = `<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylsheet" href="main.css" />
+        <title>Document</title>
+    </head>
+    <body>
+        
+        <script src="app.js"></script>
+    </body>
+</html>`
 
 console.log('Creating project with node.js -- Loading') 
 
@@ -9,28 +23,40 @@ const createDirError = error => {
     if(error){
         console.log('An error occured: ', error)
     } else {
-        console.log(`Your project has been created! Try <cd ${projectName}> to access your project. `)
+        console.log(`Directory Created -- ${path}`)
     }
 }
 
-const createFileError = error => {
+const createFileError = (fileName, resolve, reject) => error => {
     if(error){
         console.log('An error occured: ', error)
+        reject(error)
     } else {
-        console.log(`Your project has been created! Try <cd ${projectName}> to access your project. `)
+        resolve(`Added a file to ${path} -- ${fileName}`)
     }
 }
+
 const createDir = directory => {
     fs.mkdirSync(process.cwd() + directory, { recursive: true }, createDirError)
 }
 
 const createFile = (fileName, content) => {
-    fs.writeFile(`./${projectName}/${fileName}`, content, createFileError)
+    return new Promise( (resolve, reject) => {
+        fs.writeFile(`./${projectName}/${fileName}`, content, createFileError(fileName, resolve, reject))
+    })
 }
 
 if(projectName){
     createDir(path)
-    createFile('index.js', 'console.log(\"Hello World\")')
+    Promise.all([
+        createFile('app.js', 'console.log(\"Hello World\")'),
+        createFile('main.css',  '/* start styling with css below! */'),
+        createFile('index.html', boilerHTML)
+    ]).then(values => {
+        console.log('------ Project directory created.')
+        console.log(values)
+        console.log(`------ Your project has been created! Try <cd ${projectName}> to access your vanilla javascript project.`)
+    })
 } else {
     console.log('You must add the project name as an argument. ex. vanilla <project-name>')
 }
